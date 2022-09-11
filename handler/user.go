1f3db06fdd3d50bd880b947e993743374dc5f09a
Risda Tamam Aljava <risda123tamam@student.esaunggul.ac.id>
2022-09-12 00:29:00 +0700

    Upload Avatar Endpoint

--- handler/user.go
+++ handler/user.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 	"task-5-vix-btpns-RisdaTamamAljava/helper"
 	"task-5-vix-btpns-RisdaTamamAljava/user"
@@ -107,5 +108,48 @@ func (h *userHandler) CheckEmailAvailability(c *gin.Context) {
 	}
 
 	response := helper.APIResponse(metaMessage, http.StatusOK, "success", data)
+	c.JSON(http.StatusOK, response)
+}
+
+func (h *userHandler) UploadAvatar(c *gin.Context) {
+
+	file, err := c.FormFile("avatar")
+
+	if err != nil {
+		data := gin.H{"is_uploaded": false}
+		response := helper.APIResponse("Failed to upload avatar image", http.StatusBadRequest, "error", data)
+		
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	//* Harus dapat dari JWT
+	userID := 1
+
+	pathAvatar := fmt.Sprintf("images/avatar/%d-%s", userID, file.Filename)
+
+	err = c.SaveUploadedFile(file, pathAvatar)
+
+	if err != nil {
+		data := gin.H{"is_uploaded": false}
+		response := helper.APIResponse("Failed to upload avatar image", http.StatusBadRequest, "error", data)
+		
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	_, err = h.userService.SaveAvatar(userID, pathAvatar)
+
+	if err != nil {
+		data := gin.H{"is_uploaded": false}
+		response := helper.APIResponse("Failed to upload avatar image", http.StatusBadRequest, "error", data)
+		
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	data := gin.H{"is_uploaded": true}
+	response := helper.APIResponse("Avatar successfully uploaded", http.StatusOK, "success", data)
+	
 	c.JSON(http.StatusOK, response)
 }
--- main.go
+++ main.go
@@ -30,6 +30,7 @@ func main() {
 	api.POST("/users/register", userHandler.RegisterUser)
 	api.POST("/users/login", userHandler.LoginUser)
 	api.POST("/users/email_checkers", userHandler.CheckEmailAvailability)
+	api.POST("/avatars", userHandler.UploadAvatar)
 
 	router.Run()
 }
